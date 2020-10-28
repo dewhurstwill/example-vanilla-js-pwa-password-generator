dbe3c82796e7053ca4495747cc54d004c64c28aa
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2020-10-28 23:17:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+// Slider style properties
 const sliderProps = {
 	fill: "#0B1EDF",
 	background: "rgba(255, 255, 255, 0.214)",
 };
 
+// Get slider 
 const slider = document.querySelector(".range__slider");
-
+// Get slider value (length of password to generate)
 const sliderValue = document.querySelector(".length__title");
 
+// Update slider
 slider.querySelector("input").addEventListener("input", event => {
 	sliderValue.setAttribute("data-length", event.target.value);
 	applyFill(event.target);
 });
-
 applyFill(slider.querySelector("input"));
-
 function applyFill(slider) {
 	const percentage = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
 	const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${sliderProps.background} ${percentage +
@@ -22,6 +23,7 @@ function applyFill(slider) {
 	sliderValue.setAttribute("data-length", slider.value);
 }
 
+// Random Generator Functions
 const randomFunc = {
 	lower: () => String.fromCharCode(Math.floor(Math.random() * 26) + 97),
 	upper: () => String.fromCharCode(Math.floor(Math.random() * 26) + 65),
@@ -34,6 +36,7 @@ const randomFunc = {
 
 const = secureMathRandom => window.crypto.getRandomValues(new Uint32Array(1))[0] / (Math.pow(2, 32) - 1);
 
+// Get other elements by id
 const resultEl = document.getElementById("result");
 const lengthEl = document.getElementById("slider");
 const uppercaseEl = document.getElementById("uppercase");
@@ -57,6 +60,7 @@ window.addEventListener("resize", e => {
 	};
 });
 
+// Has the user clicked the generate button
 generateBtn.addEventListener("click", () => {
 	const length = +lengthEl.value;
 	const hasLower = lowercaseEl.checked;
@@ -67,7 +71,7 @@ generateBtn.addEventListener("click", () => {
 	resultEl.innerText = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
 });
 
-
+// Generate password function
 function generatePassword(length, lower, upper, number, symbol) {
 	let generatedPassword = "";
 	const typesCount = lower + upper + number + symbol;
@@ -81,14 +85,12 @@ function generatePassword(length, lower, upper, number, symbol) {
 			generatedPassword += randomFunc[funcName]();
 		});
 	}
-	return generatedPassword.slice(0, length)
-									.split('').sort(() => Math.random() - 0.5)
-									.join('');
+	return generatedPassword.slice(0, length).split('').sort(() => Math.random() - 0.5).join('');
 }
 
-// function that handles the checkboxes state, so at least one needs to be selected. The last checkbox will be disabled.
+// Function that handles the checkboxes state, so at least one needs to be selected. The last checkbox will be disabled.
 function disableOnlyCheckbox(){
-	let totalChecked = [uppercaseEl, lowercaseEl, numberEl, symbolEl].filter(el => el.checked)
+	let totalChecked = [uppercaseEl, lowercaseEl, numberEl, symbolEl].filter(el => el.checked);
 	totalChecked.forEach(el => {
 		if(totalChecked.length == 1){
 			el.disabled = true;
@@ -98,8 +100,4 @@ function disableOnlyCheckbox(){
 	})
 }
 
-[uppercaseEl, lowercaseEl, numberEl, symbolEl].forEach(el => {
-	el.addEventListener('click', () => {
-		disableOnlyCheckbox()
-	})
-})
+[uppercaseEl, lowercaseEl, numberEl, symbolEl].forEach(element => { element.addEventListener('click', () => { disableOnlyCheckbox() }); })
